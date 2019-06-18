01d681837b1a525d4b937f52ec3966fa2bb06907
王传 <a125598016@gmail.com>
2019-06-18 17:32:48 +0800

    =pwa

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, OnDestroy {
+  private unsub: Subscription;
+
+  constructor(private sw: SwUpdate) {
+
+  }
+
+  ngOnInit(): void {
+    this.unsub = this.sw.available.subscribe((item: UpdateAvailableEvent) => {
+      if (confirm(item.current.appData["msg"])) {
+        window.location.reload()
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.unsub.unsubscribe()
+  }
 }
